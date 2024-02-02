f7c64853669aaffb004d3926f975a2811beb32c7
Niks Birzgalis <75217926+birzgalisn@users.noreply.github.com>
2024-02-03 01:16:39 +0200

    Update neon example variables

--- src/neon.mjs
+++ src/neon.mjs
@@ -3,9 +3,9 @@ import { neon, neonConfig } from "@neondatabase/serverless";
 const { POSTGRES_URL, NEON_PROXY_HOST, NEON_PROXY_PORT } = process.env;
 
 neonConfig.fetchEndpoint = function (host) {
-  const isNeonProxy = host === NEON_PROXY_HOST;
-  const protocol = isNeonProxy ? "http" : "https";
-  const port = isNeonProxy ? NEON_PROXY_PORT : 443;
+  const isProxyEnabled = host === NEON_PROXY_HOST;
+  const protocol = isProxyEnabled ? "http" : "https";
+  const port = isProxyEnabled ? NEON_PROXY_PORT : 443;
   return `${protocol}://${host}:${port}/sql`;
 };
 
